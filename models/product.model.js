b1cd98d847ede1a6e76415e4ac98839ab8a9b132
Akos Szabo-Gali <szabogaliakos@gmail.com>
2023-12-12 11:55:29 +0100

    Express admin order endpoints validation

--- models/product.model.js
+++ models/product.model.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
+const Category = require('./category.model');
+const Property = require('./property.model');
+const Review = require('./review.model');
+
 const ProductSchema = new Schema({
   categoryID: {
     type: mongoose.Schema.Types.ObjectId,
--- routes/admin/orderRoutes.js
+++ routes/admin/orderRoutes.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const orderController = require('../../controllers/admin/orderController');
+const orderValidator = require('../../validators/admin/orderValidator');
 
-router.route('/').get(orderController.getAllOrders);
+router
+  .route('/')
+  .get(orderController.getAllOrders)
+  .post(orderValidator.validateOrder, orderController.createOrder);
 
 router
   .route('/:id')
   .get(orderController.getOrder)
-  .patch(orderController.updateOrder)
+  .patch(orderValidator.validateOrder, orderController.updateOrder)
   .delete(orderController.deleteOrder);
 
 module.exports = router;
